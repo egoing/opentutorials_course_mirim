ae925beffbe216aeb8b0054dde17ede985bb61ca
egoing <egoing@gmail.com>
2016-02-18 18:24:37 +0900

    조건문을 통해서 id가 없을 때 예외처리

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
-		<h1><a href="index.html">작심5일</a></h1>
+		<h1><a href="index.php">작심5일</a></h1>
 		<ol>
 			<li><a href="index.php?id=1">HTML</a></li>
 			<li><a href="index.php?id=2">CSS</a></li>
@@ -14,25 +14,13 @@
 		</ol>
 		<div id="article">
 			<?php
-      $id = $_GET['id'];
-      if($id == 1){
-        print('
-        <h2>HTML</h2>
-        HTML이란 웹페이지를 만드는 언어이다.
-        ');
-      }
-      if($id == 2){
-        print('
-        <h2>CSS</h2>
-        CSS이란 웹페이지를 디자인 하는 언어이다.
-        ');
-      }
-      if($id == 3){
-        print('
-        <h2>JavaScript</h2>
-        JS이란 웹페이지를 제어하는 언어이다.
-        ');
-      }
+			if(empty($_GET['id'])) {
+				print("<h2>Welcome!!!!</h2>");
+			} else {
+				$id = $_GET['id'];
+	      $data = file_get_contents("{$id}.txt");
+				print($data);
+			}
       ?>
 		</div>
 </body>
